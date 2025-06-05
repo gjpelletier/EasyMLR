2ed66c1930b5ffe0096d8a4889cf2ecc9852e4f0
Greg Pelletier <gjpelletier@users.noreply.github.com>
2025-06-05 06:43:20 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import sys
+sys.path.insert(0, ".")
+from EasyMLR import __version__
+
+setup(
+    name='EasyMLR',
+    version=__version__,
+    author='Greg Pelletier',
+    py_modules=['EasyMLR'], 
+    install_requires=[
+        'numpy','pandas','statsmodels',
+        'scikit-learn','tabulate','matplotlib',
+        'xgboost','lightgbm','mlxtend','optuna'],
+)
+
